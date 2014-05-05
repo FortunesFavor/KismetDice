2cb854b122157bfdd50a957d37326b61c211cae0
Alexander Walters <tritium@sdamon.com>
2014-05-05 06:02:58 -0400

    initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+Mindy's Dice
+============
+
+A ZNC module, written in Python to provide Dice for a custom RP system.
+
+To install, copy the entire package (dice/) into the ZNC module directory.
---
+++ dice/__init__.py
@@ -0,0 +1,98 @@
+import random
+
+import znc
+import dice.prettytable
+
+class dice(znc.Module):
+    description = 'Dice for Mindy\'s game'
+
+    def nv_add(self, channel):
+        self.nv[channel] = '1'
+
+    def nv_del(self, channel):
+        if channel in self.nv:
+            del self.nv[channel]
+
+    def nv_list(self):
+        return list(self.nv.keys())
+
+    def cmd_add(self, line):
+        try:
+            channel = line.split(' ')[0]
+        except IndexError:
+            self.PutModule('Missing argument')
+        else:
+            if channel in self.nv_list():
+                self.PutModule('Channel already enabled')
+            else:
+                self.nv_add(channel)
+                self.PutModule('Channel enabled')
+
+    def cmd_del(self, line):
+        try:
+            channel = line.split(' ')[0]
+        except IndexError:
+            self.PutModule('Missing argument')
+        else:
+            if channel in self.nv_list():
+                self.nv_del(channel)
+                self.PutModule('Channel disabled')
+            else:
+                self.PutModule('Channel not enabled')
+
+    def cmd_list(self, line):
+        chans = self.nv_list()
+        if chans:
+            pt = prettytable.PrettyTable(('Channel',))
+            pt.align = 'l'
+            for chan in chans:
+                pt.add_row((chan,))
+            for line in pt.get_string(sortby='Channel').splitlines():
+                self.PutModule(line)
+        else:
+            self.PutModule('No channels enabled')
+
+    def cmd_help(self, line):
+        headers = "Command Arguments Description".split()
+        pt = prettytable.PrettyTable(headers)
+        pt.align = 'l'
+        pt.add_row(['Add', '<chan>', 'Enable dice in <chan>'])
+        pt.add_row(['Del', '<chan>', 'Disable dice in <chan>'])
+        pt.add_row(['List', '', ''])
+        pt.add_row(['Help', '', 'Generate this output'])
+        out = pt.get_string(sortby='Command').splitlines()
+        for line in out:
+            self.PutModule(line)
+
+    def OnModCommand(self, line):
+        command, _, args = line.partition(' ')
+        dispatch = {
+            'add': self.cmd_add,
+            'del': self.cmd_del,
+            'list': self.cmd_list,
+            'help': self.cmd_help,
+        }
+        if command.lower() in dispatch:
+            dispatch[command.lower()](args)
+        else:
+            self.PutModule('Unknown command.  Try Help.')
+
+    def OnChanMsg(self, nick, chan, message):
+        message = str(message).split(' ')
+        if message[0] == '!roll' and chan.GetName() in self.nv_list():
+            try:
+                target_number = int(message[1])
+            except (ValueError, IndexError) as exp:
+                target_number = 20
+            roll = random.randint(1, 20)
+            success = roll <= target_number
+            success_str = 'Success' if success else 'Failure'
+            template = '{0} rolled {1} vs. {2}: {3}'
+            output = template.format(
+                nick.GetNick(),
+                roll,
+                target_number,
+                success_str,
+            )
+            self.PutIRC('PRIVMSG {0} :{1}'.format(chan.GetName(), output))
+        return znc.CONTINUE
